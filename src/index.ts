496effcd73f4fa0c2d0db6b6676bea1a9d5feeb2
PCXPY <pc@wmpcxpy.com>
2017-11-16 09:40:24 -0600

    [Update] set each head point to different edge

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import * as fs from 'fs';
 import generator from './generator';
 import svgPopper from './svgPopper';
-import { locationPointOne } from './pointLocation';
+import location from './pointLocation';
 
 export default function (username: string) {
     const gen = new generator(username);
     const svg = new svgPopper();
-    const point1 = locationPointOne(12200);
-    const point2 = locationPointOne(71232);
-    const point3 = locationPointOne(5241);
-    fs.writeFileSync('test.html', svg.setSize(100, 100).rect(point1, point2, point3, "red").flush());
+    const loc = new location();
+
+    const point1 = loc.getPoint(gen.getMedium(0, 6));
+    const point2 = loc.getPoint(gen.getMedium(6, 12));
+    const point3 = loc.getPoint(gen.getMedium(12, 18));
+    fs.writeFileSync(
+        'test.html',
+        svg.
+            setSize(100, 100).
+            reset().
+            rect(point1, point2, point3, "red").
+            flush()
+    );
 }
--- src/pointLocation.ts
+++ src/pointLocation.ts
@@ -3,26 +3,58 @@ import point from './point';
 const EDGELENGTH: number = 480;
 const EDGELENGTHTOTAL: number = EDGELENGTH * 4;
 
-export const locationPointOne = function (key: number): point {
-    if (key <= EDGELENGTHTOTAL) throw new Error("key size too small");
-    let temp: number = key % EDGELENGTHTOTAL;
-    let whichEdge: number = Math.floor(temp / EDGELENGTH);
-    let lengthLeft: number = temp % EDGELENGTH;
-    let resultPoint: point;
-    console.log(whichEdge);
-    switch (whichEdge) {
-        case 0:
-            resultPoint = { x: lengthLeft, y: 0 };
-            break;
-        case 1:
-            resultPoint = { x: EDGELENGTH, y: lengthLeft };
-            break;
-        case 2:
-            resultPoint = { x: EDGELENGTH - lengthLeft, y: EDGELENGTH }
-            break;
-        default:
-            resultPoint = { x: 0, y: EDGELENGTH - lengthLeft };
+class location {
+
+    private queue: Array<number>;
+
+    public constructor() {
+        this.queue = [];
     }
 
-    return resultPoint;
+    public getPoint(key: number): point {
+        if (key <= EDGELENGTHTOTAL) throw new Error("key size too small");
+        let temp: number = key % EDGELENGTHTOTAL;
+        let whichEdge: number = Math.floor(temp / EDGELENGTH);
+        let lengthLeft: number = temp % EDGELENGTH;
+        let resultPoint: point;
+        while (this.checkQueue(whichEdge)) {
+            if (whichEdge >= 3) {
+                whichEdge = 0;
+            } else {
+                whichEdge++;
+            }
+        }
+        switch (whichEdge) {
+            case 0:
+                resultPoint = { x: lengthLeft, y: 0 };
+                break;
+            case 1:
+                resultPoint = { x: EDGELENGTH, y: lengthLeft };
+                break;
+            case 2:
+                resultPoint = { x: EDGELENGTH - lengthLeft, y: EDGELENGTH }
+                break;
+            case 3:
+                resultPoint = { x: 0, y: EDGELENGTH - lengthLeft };
+                break;
+            default:
+        }
+        this.queue.push(whichEdge);
+        return resultPoint;
+    }
+
+    public checkQueue(edge: number): boolean {
+        for (let i: number = 0; i < this.queue.length; i++) {
+            if (this.queue[i] == edge) return true;
+        }
+        return false;
+    }
+
+    public clear(): location {
+        this.queue = [];
+        return this;
+    }
+
 }
+
+export default location;
