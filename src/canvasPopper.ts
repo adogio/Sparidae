460cb993f0547f6597aa296c34fccbf359714437
WMXPY <wm@mengw.io>
2017-11-17 14:31:25 -0600

    canvas popper

---
+++ src/canvasPopper.ts
@@ -0,0 +1,27 @@
+import point from './point.interface';
+import popper from './popper.interface';
+
+class canvasPopper implements popper {
+
+    private display: string;
+
+    public constructor(display: string) {
+        this.display = display;
+    }
+
+    public rect(point1: point, point2: point, point3: point, fill: string): canvasPopper {
+        return this;
+    }
+
+    public setSize(width: number, height: number): canvasPopper {
+        return this;
+    }
+
+    public flush(): string {
+        return "";
+    }
+
+    public reset(): canvasPopper {
+        return this;
+    }
+}
