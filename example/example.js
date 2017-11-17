23ddd8a5ffc8b1c91190a60fc1e375d4eb3ad36c
WMXPY <wm@mengw.io>
2017-11-17 00:50:47 -0600

    three digit name parser

--- example/example.js
+++ example/example.js
@@ -1,6 +1,6 @@
 let a = require('../dist/index').default;
 let fs = require('fs');
 
-let b = a("DAOIJDOIAS");
+let b = a("PCXPY");
 
 fs.writeFileSync('test.html', b);
--- src/nameParser.ts
+++ src/nameParser.ts
@@ -15,12 +15,58 @@ class nameParser {
         switch (arr.length) {
             case 0:
                 return "**";
+
             case 1:
                 return this.username.substring(0, 1).toUpperCase() + this.username.substring(this.username.length - 1, this.username.length).toLowerCase();
+
             case 2:
                 return arr[1].substring(0, 1).toUpperCase() + arr[0].substring(0, 1).toUpperCase();
+
             default:
                 return arr[0].substring(0, 1).toUpperCase() + arr[arr.length - 1].substring(0, 1).toUpperCase();
+
+        }
+    }
+
+    public getThreeDigitResult(): string {
+        let arr: Array<string> = this.username.split(" ");
+        switch (arr.length) {
+            case 0:
+                return "***";
+
+            case 1:
+                if (this.username.length < 3) {
+                    return this.username.substring(0, 1).toUpperCase() +
+                        this.username.substring(this.username.length - 1, this.username.length).toLowerCase() + "*";
+                }
+                return this.username.substring(0, 1).toUpperCase() +
+                    this.username.substring(this.username.length - 2, this.username.length).toLowerCase();
+
+            case 2:
+                if (arr[0].length < 2) {
+                    if (arr[1].length < 1) {
+                        return (arr[0].length === 1 ? (arr[0].toUpperCase() + "*") : "**") + "*";
+                    } else if (arr[1].length < 2) {
+                        return (arr[0].length === 1 ? (arr[0].toUpperCase() + "*") : arr[1].toUpperCase) + "*";
+                    }
+                    return arr[0].substring(0, 1).toUpperCase() +
+                        arr[1].substring(0, 1).toUpperCase() +
+                        arr[1].substring(arr[1].length - 1, arr[1].length).toLowerCase();
+                }
+
+                return arr[0].substring(0, 1).toUpperCase() +
+                    arr[0].substring(1, 2).toLowerCase() +
+                    arr[1].substring(0, 1).toUpperCase();
+
+            case 3:
+                return arr[0].substring(0, 1).toUpperCase() +
+                    arr[1].substring(0, 1).toLowerCase() +
+                    arr[2].substring(0, 1).toUpperCase();
+
+            default:
+                return arr[0].substring(0, 1).toUpperCase() +
+                    arr[1].substring(0, 1).toUpperCase() +
+                    arr[arr.length - 1].substring(0, 1).toUpperCase();
         }
     }
 
