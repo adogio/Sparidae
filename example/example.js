4ca93e8749db4af28ade040e691d5a6188c0adeb
WMXPY <wm@mengw.io>
2017-11-20 19:14:42 -0600

    add example

--- example/example.js
+++ example/example.js
@@ -2,7 +2,9 @@ let a = require('../dist/index').default;
 let fs = require('fs');
 
 let b = a("PCXPY", {
-    long: true
+    long: true,
+    width: 900,
+    height: 500
 });
 
 fs.writeFileSync('test.html', b);
